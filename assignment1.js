6449723e3e4fb48fd85ffe8a693307f369022ce8
akshitha5555 <110324262+akshitha5555@users.noreply.github.com>
2022-12-08 18:53:42 +0530

    assignment

---
+++ assignment1.js
@@ -0,0 +1,10 @@
+function calculate(a,b){
+    console.log("Addition "+(a+b));
+    console.log("Subtraction "+(a-b));
+    console.log("Multiplication "+(a*b));
+    console.log("Division  "+(a?b));
+}
+
+let a=10
+let b=15
+calculate(a,b)
